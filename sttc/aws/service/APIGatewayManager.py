d914a872450891b949e93d3eabafabced1adaa65
Sebastien Coste <teddy.coste@gmail.com>
2017-05-13 19:31:54 +0200

    update create behaviors to create if not exists behaviors

--- sttc/aws/service/APIGatewayManager.py
+++ sttc/aws/service/APIGatewayManager.py
@@ -5,6 +5,7 @@ Created on 13 mai 2017
 '''
 from sttc.aws.config.ConfigProvider import ConfigProvider
 import boto3
+from os.path import join
 
 class APIGatewayManager:
 
@@ -22,31 +23,41 @@ class APIGatewayManager:
        
     def createAPI(self, conf):
         
-        response = self.gateway.create_rest_api(
-            name=conf["name"],
-            description='auto generated API',
-            version=conf["version"]
-        )
-        
-        conf['apiId'] = response['id']
-        code = response['ResponseMetadata']['HTTPStatusCode']
-        print(self.t.getMessage("createAPI") + " " + conf['apiId'])
-        
+        api = self.getApiByNameOrId(name=conf['name'])
+        if api == None:
+            response = self.gateway.create_rest_api(
+                name=conf["name"],
+                description='auto generated API',
+                version=conf["version"]
+            )
+            conf['apiId'] = response['id']
+            code = response['ResponseMetadata']['HTTPStatusCode']
+            print(self.t.getMessage("createAPI") + " " + conf['apiId'])
+        else:
+            if not 'apiId' in conf.keys():
+                conf['apiId'] = api['id']
+                
         root = self.getResourceByPath(conf['apiId'], "/")
         print(self.t.getMessage("createResource") + " " + root['id'])
-        self.createResource(conf['resource'], conf['apiId'], root['id'])
+        self.createResource(conf['resource'], conf['apiId'], root['id'], "/")
         
-    def createResource(self, confResource, apiId, parentId):
         
-        response = self.gateway.create_resource(
-            restApiId= apiId,
-            parentId= parentId,
-            pathPart= confResource['pathPart']
-        )
+    def createResource(self, confResource, apiId, parentId, absoluteParentPath):
         
-        resourceId = response['id']
-        print(self.t.getMessage("createResource") + " " + resourceId)
+        absolutePath = join(absoluteParentPath, confResource['pathPart'])
+        resource = self.getResourceByPath(apiId, absolutePath)
+        resourceId = None
         
+        if resource == None:
+            response = self.gateway.create_resource(
+                restApiId= apiId,
+                parentId= parentId,
+                pathPart= confResource['pathPart']
+            )
+            resourceId = response['id']
+            print(self.t.getMessage("createResource") + " " + resourceId)
+        else:
+            resourceId = resource['id']
         
         if "method" in confResource.keys():
             for method in confResource['method']:
@@ -54,47 +65,101 @@ class APIGatewayManager:
                 self.createMethod(method, apiId, resourceId)
             
         if "resource" in confResource.keys():
-            self.createResource(confResource["resource"], apiId, resourceId)
+            self.createResource(confResource["resource"], apiId, resourceId, absolutePath)
+        
         
     def createMethod(self, confMethod, apiId, resourceId):
         
-        authId = ""
-        if "authorizerId" in confMethod.keys():
-            authId = confMethod['authorizerId']
-        operationName = ""
-        if "operationName" in confMethod.keys():
-            operationName = confMethod['operationName']
+        method = self.getMethodOfResource(apiId, resourceId, confMethod['httpMethod'])
+        createMethod = method == None or method['authorizationType'] != confMethod['authorizationType']
+        if not createMethod:
+            if "authorizerId" in method.keys() and not "authorizerId" in confMethod.keys():
+                createMethod = True
+            if not "authorizerId" in method.keys() and "authorizerId" in confMethod.keys():
+                createMethod = True
+            
+        if createMethod:
+            if method != None:
+                self.removeMethod(confMethod['httpMethod'], apiId, resourceId)
+            
+            authId = ""
+            if "authorizerId" in confMethod.keys():
+                authId = confMethod['authorizerId']
+            
+            response = self.gateway.put_method(
+                restApiId= apiId,
+                resourceId= resourceId,
+                httpMethod= confMethod['httpMethod'],
+                authorizationType= confMethod['authorizationType'],
+                authorizerId=authId,
+                apiKeyRequired=False
+            )
+            print(self.t.getMessage("createMethod") + " " + resourceId + " : " +confMethod['httpMethod'])
         
-        response = self.gateway.put_method(
-            restApiId= apiId,
-            resourceId= resourceId,
-            httpMethod= confMethod['httpMethod'],
-            authorizationType= confMethod['authorizationType'],
-            authorizerId=authId,
-            apiKeyRequired=False
+        
+        
+        
+    def removeMethod(self, httpMethod, apiId, resourceId):
+        
+        self.gateway.delete_method(
+            restApiId=apiId,
+            resourceId=resourceId,
+            httpMethod=httpMethod
         )
-        
-        
-        print(self.t.getMessage("createMethod") + " " + resourceId + " : " +confMethod['httpMethod'])
-        
     
-    def getResourceByPath(self, rest_api_id, path):
+    
+    def getResourceByPath(self, apiId, path):
 
         resource = None
-        resource_items = self.gateway.get_resources(restApiId=rest_api_id, limit=500)['items']
+        try:
+            resourceItems = self.gateway.get_resources(restApiId=apiId, limit=500)['items']
+        
+            for item in resourceItems:
+                if item['path'] == path:
+                    resource = item
+                    break
+        
+            return resource
+        except:
+            return None
+        
     
-        for item in resource_items:
-            if item['path'] == path:
-                resource = item
-                break
+    def getApiByNameOrId(self, name=None, apiId=None ):
+        
+        if name == None and apiId == None:
+            return False
+        
+        response = self.gateway.get_rest_apis()
+        
+        for item in response['items']:
+            if name != None:
+                if item['name'] == name:
+                    return item
+            if apiId != None:
+                if item['id'] == apiId:
+                    return item
+        
+        return None
+                
     
-        return resource
+    def getMethodOfResource(self, apiId, resourceId, httpMethod):
+        
+        try:
+            return self.gateway.get_method(
+                restApiId= apiId,
+                resourceId= resourceId,
+                httpMethod= httpMethod
+            )
+        except:
+            return None
         
         
+    def linkMethodAndLambda(self, apiName, path, httpMethod, confLambda):
         
-        
-        
-        
+        api = self.getApiByName(apiName)
+        resource = self.getResourceByPath(api['id'], path)
+        method = self.getMethodOfResource(api['id'], resource['id'], httpMethod)
+        #TODO next
         
         
         
